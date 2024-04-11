22636e7ab43ba3a81075d52010a14c736838470f
RadouaneBaba <radoine.baba04@gmail.com>
2024-04-11 19:35:47 +0000

    tesst

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -21,7 +21,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+    return `${this._amount} ${this._currency._name} (${this._currency.code})`;
   }
 
   static converPrice(amount, conversionRate) {
