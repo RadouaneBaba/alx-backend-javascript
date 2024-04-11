67aec7f447d80af19cdc73fc8606707241e08d53
RadouaneBaba <radoine.baba04@gmail.com>
2024-04-11 14:17:52 +0000

    corr

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,11 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name != "string") {
+      throw new TypeError("Name must be a string");
+    }
+    if (typeof length != "number") {
+      throw new TypeError("Length must be a number");
+    }
     this._name = name;
     this._length = length;
     this._students = students;
@@ -18,10 +24,16 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
+    if (typeof newName != "string") {
+      throw new TypeError("Name must be a string");
+    }
     this._name = newName;
   }
 
   set length(newLength) {
+    if (typeof newLength != "number") {
+      throw new TypeError("Length must be a number");
+    }
     this._length = newLength;
   }
 
