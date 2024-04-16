0fea012333c18f57f7d810cb6494704065861423
Radouane Baba <radoine.baba04@gmail.com>
2024-04-16 18:27:17 +0100

    test

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(newMap) {
+  if (newMap instanceof Map === false) {
+    throw new Error('Cannot process');
+  }
   for (const elem of newMap.keys()) {
     if (newMap.get(elem) === 1) {
       newMap.set(elem, 100);
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,10 +5,12 @@ export default function cleanSet(newSet, startString) {
   let finalString = '';
   for (const elem of newSet) {
     if (elem.startsWith(startString)) {
-      finalString += elem.substring(startString.length);
+      finalString += elem.slice(startString.length);
       finalString += '-';
     }
   }
-  finalString = finalString.slice(0, -1);
+  if (finalString.length > 0) {
+    finalString = finalString.slice(0, -1);
+  }
   return finalString;
 }
