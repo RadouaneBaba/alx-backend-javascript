c55a12e5bca6a577465ea5dc01e87babb71a1486
Radouane Baba <radoine.baba04@gmail.com>
2024-06-04 15:09:17 +0100

    fix

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf-8');
+    const data = fs.readFileSync(path, 'utf8');
     const fields = {};
     const students = data.split('\n');
     console.log(`Number of students: ${students.length - 1}`);
