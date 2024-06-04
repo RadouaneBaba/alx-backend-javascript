158b0b73a14d382ede42023c343405d10888fe9f
Radouane Baba <radoine.baba04@gmail.com>
2024-06-04 20:46:57 +0100

    http_express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = 1245
+const fs = require('fs');
+
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+  });
+
+app.get('/students', (req, res) => {
+    fs.promises.readFile(process.argv[2], 'utf8')
+      .then((data) => {
+        const students = data.split('\r\n');
+        const fields = {};
+        const studentsCount = students.filter((student) => student !== '').length - 1;
+        let resText = `This is the list of our students\nNumber of students: ${studentsCount}\n`;
+        for (const student of students) {
+          if (student !== '') {
+            const line = student.split(',');
+            if (!fields[line[3]]) fields[line[3]] = [line[0]];
+            else fields[line[3]].push(line[0]);
+          }
+        }
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            resText += `Number of students in ${key}: ${value.length}. List: ${fields[key].join(', ')}\n`;
+          }
+        }
+        res.send(resText);
+      }).catch(() => {
+        res.send('Cannot load the database');
+      });
+});
+
+app.listen(port);
+
+module.exports = app;
