fbe004e4bcbdc36f4d8764e5c5a78dcfd58d8e2b
Radouane Baba <radoine.baba04@gmail.com>
2024-06-04 14:48:52 +0100

    test

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,28 +1,31 @@
 const fs = require('fs');
+
 function countStudents(path) {
-  fs.readFile(path, "utf-8", (err, data) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
     if (err) {
       throw new Error('Cannot load the database');
     }
-    const fields = {}
+    const fields = {};
     const students = data.split('\n');
-    console.log('Number of students:', students.length - 1)
+    console.log(`Number of students: ${students.length - 1}`);
     for (const student of students) {
-      if (student == '') continue;
-      const data = student.split(',')
-      if (!fields[data[3]]) fields[data[3]] = [data[0]]
-      else fields[data[3]].push(data[0])
+      if (student !== '') {
+        const data = student.split(',');
+        if (!fields[data[3]]) fields[data[3]] = [data[0]];
+        else fields[data[3]].push(data[0]);
+      }
     }
     for (const [key, value] of Object.entries(fields)) {
-      if (key == 'field') continue;
-      let listStr = '';
-      for (let i = 0; i < fields[key].length; i++) {
-        listStr += fields[key][i];
-        if (i < fields[key].length - 1) listStr += ', ';
+      if (key !== 'field') {
+        let listStr = '';
+        for (let i = 0; i < fields[key].length; i += 1) {
+          listStr += fields[key][i];
+          if (i < fields[key].length - 1) listStr += ', ';
+        }
+        console.log(`Number of students in ${key}: ${value.length}. List: ${listStr}`);
       }
-      console.log(`Number of students in ${key}: ${value.length}. List: ${listStr}`);
     }
-  })
+  });
 }
 
 module.exports = countStudents;
