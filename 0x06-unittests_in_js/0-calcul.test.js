49b36b414d5009091d8b3c9da2ae269ffe313645
Radouane Baba <radoine.baba04@gmail.com>
2024-06-06 17:34:59 +0100

    test

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js')
+describe('calcul', function () {
+  it('normal addition test', function () {
+    const result = calculateNumber(1, 3);
+    assert.equal(result, 4);
+  });
+  it('addition with one non rounded number', function () {
+    const result = calculateNumber(1, 3.7);
+    assert.equal(result, 5);
+  });
+  it('addition with two non rounded numbers', function () {
+    const result = calculateNumber(1.2, 3.7);
+    assert.equal(result, 5);
+  });
+  it('test .5 numbers', function () {
+    const result = calculateNumber(1.5, 3.7);
+    assert.equal(result, 6);
+  });
+});
