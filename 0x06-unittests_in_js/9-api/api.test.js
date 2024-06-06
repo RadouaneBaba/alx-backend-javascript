6d36cc7c65fd1f3095f9a122d2b07afc6ec8e521
Radouane Baba <radoine.baba04@gmail.com>
2024-06-06 22:30:13 +0100

    test3

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,39 @@
+const chai = require('chai');
+const expect = chai.expect;
+const request = require('request');
+
+describe('index page', function () {
+  it('correct status', function () {
+    request('http://localhost:7865', function (err, resp, body) {
+      expect(resp.statusCode).to.equal(200);
+    });
+  });
+  it('correct body', function () {
+    request('http://localhost:7865', function (err, resp, body) {
+      expect(body).to.equal('Welcome to the payment system');
+    });
+  });
+  it('invalid route', function () {
+    request('http://localhost:7865/test', function (err, resp, body) {
+      expect(resp.statusCode).to.equal(404);
+    });
+  });
+});
+
+describe('cart page', function () {
+  it('correct status cart', function () {
+    request('http://localhost:7865/cart/5', function (err, resp, body) {
+      expect(resp.statusCode).to.equal(200);
+    });
+  });
+  it('correct body cart', function () {
+    request('http://localhost:7865/cart/5', function (err, resp, body) {
+      expect(body).to.equal('Payment methods for cart 5');
+    });
+  });
+  it('invalid type', function () {
+    request('http://localhost:7865/cart/test', function (err, resp, body) {
+      expect(resp.statusCode).to.equal(404);
+    });
+  });
+});
