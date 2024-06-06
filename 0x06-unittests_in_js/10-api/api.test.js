4a8a3a41a29aa2e66f9e3f99ba9b752c9ef36c92
Radouane Baba <radoine.baba04@gmail.com>
2024-06-07 00:23:42 +0100

    test

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -52,3 +52,49 @@ describe('cart page', function () {
     });
   });
 });
+
+describe('login and payments', function () {
+  it('correct status', function () {
+    request('http://localhost:7865/available_payments', function (err, resp, body) {
+      expect(resp.statusCode).to.equal(200);
+    });
+  });
+  it('correct body', function () {
+    request('http://localhost:7865/available_payments', function (err, resp, body) {
+      expect(JSON.parse(body)).to.deep.equal({
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+        }
+      });
+    });
+  });
+  it('invalid route 2', function () {
+    request('http://localhost:7865/available_payments/test', function (err, resp, body) {
+      expect(resp.statusCode).to.equal(404);
+    });
+  });
+
+  it('valid login', function (done) {
+    request.post({
+      url: 'http://localhost:7865/login',
+      form: {
+        userName: 'Radouane'
+      }
+    }, function (err, resp, body) {
+      expect(body).to.equal('Welcome Radouane');
+      done();
+    });
+  });
+  it('valid login status', function (done) {
+    request.post({
+      url: 'http://localhost:7865/login',
+      form: {
+        userName: 'Radouane'
+      }
+    }, function (err, resp, body) {
+      expect(resp.statusCode).to.equal(200);
+      done();
+    });
+  });
+});
